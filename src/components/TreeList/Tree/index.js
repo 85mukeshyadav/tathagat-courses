7c5b9ca703fecdd3451928f25df05de6db8a7b06
ayusshrathore <ayusshrathore@gmail.com>
2023-04-24 21:45:00 +0530

    chore: refactor imports & open test instructions before start test

--- src/components/TreeList/Tree/index.js
+++ src/components/TreeList/Tree/index.js
@@ -1,9 +1,10 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import "./index.css";
 import { Link, useNavigate } from "react-router-dom";
-import Modal from "react-modal";
 import Score from "../../Course/Score";
+import { useDisclosure } from "@mantine/hooks";
+import { Modal, Group, Button, ScrollArea } from "@mantine/core";
+import { Checkbox } from "@mantine/core";
 // import hideNavContext from "../../../context/AllprojectsContext";
 
 const Tree = ({ data = [], setNodeClick }) => {
@@ -20,18 +21,12 @@ const Tree = ({ data = [], setNodeClick }) => {
 
 const TreeNode = ({ node, setNodeClick }) => {
 	const [childVisible, setChildVisiblity] = useState(false);
-	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [scoreOpened, scoreHooks] = useDisclosure(false);
+	const [instructionsOpened, instructionsHooks] = useDisclosure(false);
+	const [checked, setChecked] = useState(false);
 	const navigate = useNavigate();
 
 	const hasChild = node.children ? true : false;
-
-	const setModalIsOpenToFalse = () => {
-		setModalIsOpen(false);
-	};
-
-	const setModalIsOpenToTrue = () => {
-		setModalIsOpen(true);
-	};
 
 	const redirectTo = (url) => {
 		navigate(url);
@@ -40,31 +35,49 @@ const TreeNode = ({ node, setNodeClick }) => {
 	return (
 		<>
 			<Modal
-				isOpen={modalIsOpen}
-				style={{
-					content: {
-						height: "29%",
-						position: "absolute",
-						top: "25%",
-					},
-				}}
-				ariaHideApp={false}
+				opened={scoreOpened}
+				onClose={scoreHooks.close}
+				title="Score"
+				size="100%"
+				centered
+				className="h-1/2"
 			>
-				<button onClick={setModalIsOpenToFalse}>x</button>
 				<Score />
+			</Modal>
+			<Modal
+				opened={instructionsOpened}
+				onClose={instructionsHooks.close}
+				// title="Instructions"
+				size="100%"
+			>
+				<ScrollArea h={500}>
+					{/* <p className="text-2xl font-bold text-gray-700">Test Instructions</p> */}
+					<div
+						className="mt-4 px-6"
+						dangerouslySetInnerHTML={{ __html: node.instructions }}
+					/>
+				</ScrollArea>
+				<Checkbox
+					checked={checked}
+					onChange={(event) => setChecked(event.currentTarget.checked)}
+					label="I have read and understood the instructions"
+				/>
+				<Group position="right">
+					<Button
+						onClick={() => {
+							localStorage.setItem("testid", node.id);
+							redirectTo("/examination");
+						}}
+						disabled={!checked}
+						className="items-center m-2 p-2 pl-4 pr-4 rounded-md font-semibold bg-blue-400 text-gray-50"
+					>
+						<i className="fas fa-play mr-2" />
+						Begin Test
+					</Button>
+				</Group>
 			</Modal>
 			<li className="d-tree-node">
 				<div className="d-flex" onClick={(e) => setChildVisiblity((v) => !v)}>
-					{hasChild && (
-						<div
-							className={`d-inline d-tree-toggler ${
-								childVisible ? "active" : ""
-							}`}
-						>
-							<FontAwesomeIcon icon="caret-right" />
-						</div>
-					)}
-
 					<div>
 						<div
 							className="col flex justify-center d-tree-head w-max bg-blue-100 p-2 rounded-md"
@@ -100,18 +113,18 @@ const TreeNode = ({ node, setNodeClick }) => {
 													localStorage.setItem("testid", node.id);
 													redirectTo("/review");
 												}}
-												className="items-center w-32 m-2 p-1 pl-4 pr-4 disabled:cursor-not-allowed rounded-sm font-semibold bg-blue-400 text-gray-50"
+												className="items-center w-32 m-2 p-2 px-4 rounded-md font-semibold bg-blue-400 text-gray-50"
 											>
-												<i className="fas fa-eye"></i> {"  "}
+												<i className="fa fa-eye"></i> {"  "}
 												Review
 											</button>
 
 											<button
 												onClick={() => {
 													localStorage.setItem("testid", node.id);
-													setModalIsOpenToTrue();
+													scoreHooks.open();
 												}}
-												className="items-center w-32 m-2  p-1 pl-4 pr-4 disabled:cursor-not-allowed rounded-sm font-semibold bg-blue-400 text-gray-50"
+												className="items-center w-32 m-2 p-2 px-4 rounded-md font-semibold bg-blue-400 text-gray-50"
 											>
 												<i className="fas fa-trophy"></i> {"  "}
 												Score
@@ -121,7 +134,7 @@ const TreeNode = ({ node, setNodeClick }) => {
 													localStorage.setItem("testid", node.id);
 													redirectTo("/analysis");
 												}}
-												className="items-center w-32 m-2 p-1 pl-4 pr-4 disabled:cursor-not-allowed rounded-sm font-semibold bg-blue-400 text-gray-50"
+												className="items-center w-32 m-2 p-2 px-4 rounded-md font-semibold bg-blue-400 text-gray-50"
 											>
 												<i className="fas fa-chart-bar"></i> {"  "}
 												Analysis
@@ -130,12 +143,14 @@ const TreeNode = ({ node, setNodeClick }) => {
 									</>
 								) : (
 									<Link
-										to={`/examination`}
-										onClickCapture={() =>
-											localStorage.setItem("testid", node.id)
-										}
+										to={node.instructions ? "" : "/examination"}
+										onClickCapture={() => {
+											localStorage.setItem("testid", node.id);
+											node.instructions && instructionsHooks.open();
+											setChecked(false);
+										}}
 									>
-										<button className="items-center m-2 p-1 pl-4 pr-4 disabled:cursor-not-allowed rounded-sm font-semibold bg-blue-400 text-gray-50">
+										<button className="items-center w-32 m-2 p-2 px-4 rounded-md font-semibold bg-blue-400 text-gray-50">
 											<i className="fas fa-play"></i> {"  "}
 											Start Test
 										</button>{" "}
