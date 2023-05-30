372c4562e58fa18dc2fa6c2615bc755073395b30
ayusshrathore <ayusshrathore@gmail.com>
2023-05-31 00:04:22 +0530

    chore: refactor routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Examination from "./Exam/Examination";
+import Review from "./Exam/Review";
+import TestExam from "./Exam/TestExam";
+import Exam from "./Exam/examScreen";
 import Page404 from "./components/404";
-import Signin from "./components/Auth/signin";
+import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/Signup";
+import Signin from "./components/Auth/signin";
 import Blogs from "./components/Blogs";
+import Bookmarks from "./components/Bookmarks";
+import Analysis from "./components/Course/Analysis";
 import CourseDetails from "./components/Course/courseDetails";
+import CourseInfo from "./components/Course/courseInfo";
 import CourseList from "./components/Course/courselist";
 import MyCourses from "./components/Course/myCourses";
-import Homepage from "./components/Homepage";
-import Examination from "./Exam/Examination";
-import Exam from "./Exam/examScreen";
-import Review from "./Exam/Review";
-import Analysis from "./components/Course/Analysis";
-import TestExam from "./Exam/TestExam";
 import ExamFinished from "./components/ExamFinished";
+import Homepage from "./components/Homepage";
+import PaymentSuccess from "./components/PaymentSuccess";
 
 /**
  * Routes component containing routes for the whole application
@@ -24,7 +28,9 @@ const Rout = (props) => (
 		<Route exact path="signin" element={<Signin />} />
 		<Route exact path="signup" element={<Signup />} />
 		<Route exact path="courses" element={<CourseList />} />
+		<Route exact path="courses/:slug/:pkgid" element={<CourseInfo />} />
 		<Route exact path="myCourses" element={<MyCourses />} />
+		<Route exact path="bookmarks" element={<Bookmarks />} />
 		<Route
 			exact
 			path="courseDetails/:courseType"
@@ -37,7 +43,9 @@ const Rout = (props) => (
 		<Route exact path="" element={<Homepage />} />
 		<Route exact path="/blogs" element={<Blogs />} />
 		<Route exact path="/test/:pkgid/:testid/:user" element={<TestExam />} />
+		<Route exact path="/ssologin/:token/:user" element={<Login />} />
 		<Route exact path="/exam-finished" element={<ExamFinished />} />
+		<Route exact path="/payment-success" element={<PaymentSuccess />} />
 		{/* <Navigate exacts from='/' to='/login' /> */}
 		<Route exact path="*" element={<Page404 />} />
 	</Routes>
