9efed30e5ee20e95532b4042393ef3081a1dea80
ayusshrathore <ayusshrathore@gmail.com>
2023-05-30 23:58:59 +0530

    chore: add ability to make payments via razorpay

--- src/components/Course/courseInfo.js
+++ src/components/Course/courseInfo.js
@@ -1,19 +1,50 @@
 import { Group, Rating } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import ms from "ms";
 import React from "react";
 import { useParams } from "react-router-dom";
 import apiClient from "../../api/apiClient";
+import useRazorPay from "../../hooks/useRazorpay";
 import Loader from "../Loader";
 
 const CourseInfo = () => {
 	const { pkgid } = useParams();
+	const { displayRazorpay } = useRazorPay();
 
 	const { data, isLoading } = useQuery({
 		queryKey: ["package", pkgid],
 		queryFn: () => apiClient.get(`/pacakge/${pkgid}`).then((res) => res.data),
 		staleTime: ms("24h"),
 	});
+
+	const handlePayment = async (info) => {
+		console.log(info);
+		const res = await axios.post(
+			process.env.REACT_APP_API + "/create-payment",
+			{
+				amount: info.price,
+				currency: "INR",
+				receipt: "order_rcptid_11",
+			}
+		);
+		console.log(
+			"🚀 ~ file:courselist.js ~ line 28 ~ handlePayment ~ res",
+			res.data
+		);
+		const user = JSON.parse(localStorage.getItem("user_info"));
+		if (res.status === 200) {
+			displayRazorpay({
+				amount: res.data.order.amount,
+				order_id: res.data.order.id,
+				currency: res.data.order.currency,
+				packageId: info.packageId,
+				name: user?.name,
+				email: user?.email,
+				phone: user?.phone || "",
+			});
+		}
+	};
 
 	console.log("🚀 ~ file: courseInfo.js ~ line 18 ~ CourseInfo ~ data", data);
 
@@ -41,15 +72,19 @@ const CourseInfo = () => {
 						<div className="w-1/2 ml-6">
 							<button
 								onClick={() => {
-									localStorage.setItem("pkgid", data[0].packageId);
-									//window.open('https://tathagat.ccavenue.com','_blank').focus();
-									if (data[0].payment_url)
-										window.open(data[0].payment_url, "_blank").focus();
+									// localStorage.setItem("pkgid", data[0].packageId);
+									// //window.open('https://tathagat.ccavenue.com','_blank').focus();
+									// if (data[0].payment_url)
+									// 	window.open(data[0].payment_url, "_blank").focus();
+									handlePayment({
+										price: parseInt(data[0].PackagePrice),
+										packageId: data[0].packageId,
+									});
 								}}
 								className="py-4 justify-center items-center mt-5 rounded-md sm:px-6 px-4 bg-blue-500 text-white sm:text-xl text-lg font-bold hover:bg-blue-600"
 							>
 								<i className="fas fa-cart-shopping text-white mr-2"></i>
-								Enroll in Course
+								Buy Now
 							</button>
 						</div>
 					</div>
@@ -78,15 +113,19 @@ const CourseInfo = () => {
 			/>
 			<button
 				onClick={() => {
-					localStorage.setItem("pkgid", data[0].packageId);
-					//window.open('https://tathagat.ccavenue.com','_blank').focus();
-					if (data[0].payment_url)
-						window.open(data[0].payment_url, "_blank").focus();
+					// localStorage.setItem("pkgid", data[0].packageId);
+					// //window.open('https://tathagat.ccavenue.com','_blank').focus();
+					// if (data[0].payment_url)
+					// 	window.open(data[0].payment_url, "_blank").focus();
+					handlePayment({
+						price: parseInt(data[0].PackagePrice),
+						packageId: data[0].packageId,
+					});
 				}}
 				className="py-4 mb-8 justify-center items-center mt-5 rounded-md px-6 bg-blue-400 text-white text-xl font-bold hover:bg-blue-500"
 			>
 				<i className="fas fa-cart-shopping text-white mr-2"></i>
-				Enroll in Course
+				Buy Now
 			</button>
 		</div>
 	);
---
+++ src/hooks/useRazorpay.js
@@ -0,0 +1,77 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const useRazorPay = () => {
+	const navigate = useNavigate();
+	const verifyPayment = async (paymentInfo) => {
+		const res = await axios.post(
+			process.env.REACT_APP_API + "/verify-payment",
+			paymentInfo
+		);
+		console.log(
+			"🚀 ~ file:useRazorpay.js ~ line 7 ~ verifyPayment ~ res",
+			res.data
+		);
+		if (res.status === 200) {
+			axios
+				.post(process.env.REACT_APP_API + "/assignStudentToPackage", {
+					packageId: paymentInfo.packageId,
+					studentList: [
+						{
+							checked: true,
+							email_Id: localStorage.getItem("user"),
+							user_type: "student",
+						},
+					],
+				})
+				.then((res) => {
+					console.log(res.data);
+					if (res.status === 200) {
+						navigate("/payment-success");
+					}
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	};
+
+	const displayRazorpay = ({
+		amount,
+		currency,
+		order_id,
+		name,
+		email,
+		phone,
+		packageId,
+	}) => {
+		const options = {
+			key: "rzp_live_MGkjM1JRNckkm3",
+			currency: currency,
+			amount: amount,
+			name: "Tathagat",
+			description: "Course Payment",
+			order_id: order_id,
+			handler: function (response) {
+				console.log(response);
+				verifyPayment({
+					razorpay_payment_id: response.razorpay_payment_id,
+					razorpay_order_id: response.razorpay_order_id,
+					razorpay_signature: response.razorpay_signature,
+					packageId: packageId,
+				});
+			},
+			prefill: {
+				name: name,
+				email: email,
+				contact: phone,
+			},
+		};
+		const paymentObject = new window.Razorpay(options);
+		paymentObject.open();
+	};
+
+	return { displayRazorpay };
+};
+
+export default useRazorPay;
