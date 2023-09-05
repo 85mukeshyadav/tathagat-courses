5d97caaa9ef33d59f968fc8a4e6e0a0266c6bdb9
ayusshrathore <ayusshrathore@gmail.com>
2023-09-05 20:18:11 +0530

    chore: add routes for contact, privacy, terms & refund

---
+++ src/components/Privacy.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Privacy() {
+	return (
+		<>
+			<div>
+				<h1>Privacy</h1>
+			</div>
+		</>
+	);
+}
---
+++ src/components/Refund.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Refund() {
+	return (
+		<>
+			<div>
+				<h1>Refund</h1>
+			</div>
+		</>
+	);
+}
